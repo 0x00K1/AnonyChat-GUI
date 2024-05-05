6e8dec8c749f65ebd18b1226ac3a72ef9ae6bb15
0x00K1 <61327783+0x00K1@users.noreply.github.com>
2024-05-05 23:02:51 +0300

    Delete src/GUI directory

--- src/GUI/AnonyFace.java
+++
@@ -1,8 +0,0 @@
-package com.anonychat.main.GUI;
-
-public interface AnonyFace {
-    long SERIAL_VERSION_UID = 1L;
-	void initializeComponents();
-	void setupActions();
-    boolean validateInput();
-}
